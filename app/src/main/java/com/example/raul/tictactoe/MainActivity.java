cbae8da997ab3fae4f833e31f916d2938499a5d7
rlopezlu <rlopezlu@ucsc.edu>
2016-01-18 02:26:13 -0800

    added global vars

--- app/src/main/java/com/example/raul/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/raul/tictactoe/MainActivity.java
@@ -1,45 +1,51 @@
 package com.example.raul.tictactoe;
 
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.graphics.Color;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean turn = false;
+
+    ImageButton img00;
+    ImageButton img01;
+    ImageButton img02;
+    ImageButton img10;
+    ImageButton img11;
+    ImageButton img12;
+    ImageButton img20;
+    ImageButton img21;
+    ImageButton img22;
+    TextView status;
+
+    boolean turn = true;
     int curDraw = R.drawable.circle;
     int[] values = new int[9];
-    boolean autoTurn = false;
 
-    public void imgChoose(View img){
-        ImageView chooser = (ImageView) findViewById(R.id.imageButtonChoose);
+    public void turnSwitch(View img) {
         GridLayout ticTac = (GridLayout) findViewById(R.id.ticTacBoard);
-        if(turn == !false){
+        if (turn == false) {
             curDraw = R.drawable.circle;
             //need to use below because other getColor() method was deprecated
-            ticTac.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.turn1));
-            chooser.setBackgroundColor(getResources().getColor(R.color.turn1));
+            ticTac.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.pinkish));
         } else {
             curDraw = R.drawable.cross;
-            ticTac.setBackgroundColor(getResources().getColor(R.color.turn2));
-            chooser.setBackgroundColor(getResources().getColor(R.color.turn2));
+            ticTac.setBackgroundColor(getResources().getColor(R.color.bluish));
         }
-        chooser.setImageResource(curDraw);
     }
 
 
-    public void imgPress(View img){
+    public void imgPress(View img) {
 /*
+
         String Id = img.getResources().getResourceName(img.getId());//get the Id of Image Resource
         int position = Character.getNumericValue(Id.charAt(Id.length() - 1 ));//convert it to integer
 
@@ -47,24 +53,21 @@ public class MainActivity extends AppCompatActivity {
         Log.i("testing functions lol", Integer.toString(position));
 */
 
-        imgChoose(img);
-        ImageButton img00 = (ImageButton) findViewById(R.id.imageButton00);
-        ImageButton img01 = (ImageButton) findViewById(R.id.imageButton01);
-        ImageButton img02 = (ImageButton) findViewById(R.id.imageButton02);
-        ImageButton img10 = (ImageButton) findViewById(R.id.imageButton10);
-        ImageButton img11 = (ImageButton) findViewById(R.id.imageButton11);
-        ImageButton img12 = (ImageButton) findViewById(R.id.imageButton12);
-        ImageButton img20 = (ImageButton) findViewById(R.id.imageButton20);
-        ImageButton img21 = (ImageButton) findViewById(R.id.imageButton21);
-        ImageButton img22 = (ImageButton) findViewById(R.id.imageButton22);
+//        ImageButton img00 = (ImageButton)img;
 
-       // resetGame(img00, img01, img02, img10, img11, img12, img20, img21, img22);
+        if(endGame()){
+            return;
+        }
 
-        TextView status = (TextView) findViewById(R.id.Title);
+
+
         String current = img.getTag().toString();
         int curIndex = Integer.parseInt(current);
 
+
+
         if (values[curIndex] == 0) {
+            Log.i("Entered", "entered the switch statement");
             switch (current) {// used to be current
                 case "0":
                     img00.setImageResource(curDraw);
@@ -94,54 +97,63 @@ public class MainActivity extends AppCompatActivity {
                     img22.setImageResource(curDraw);
                     break;
             }
-
-            Log.i("lastTag", current);
-            Log.i("curDraw", Integer.toString(curDraw));
-
-            values[curIndex] = curDraw;
-
+            turnSwitch(img);
             turn = !turn;
         }
-        if(endGame() == true){
+        values[curIndex] = curDraw;
+        Log.i("turn?",  (turn) ? "yes tuen" : "no turn");
+        Log.i("lastTag", current);
+        Log.i("curDraw", Integer.toString(curDraw));
+
+
+        if (endGame() == true) {
+            Log.i("end?", "end condition is true");
             status.setVisibility(View.VISIBLE);
+            Log.i("end?", "end condition is true");
         }
     }// end of function
 
-    public void resetGame(View view){
-        imgPress(view);
+    public void resetGame(View view) {
         Arrays.fill(values, 0);
-        ImageButton b = (ImageButton) findViewById(R.id.imageButton01);
-        b.setImageResource(android.R.color.transparent);
+        status.setVisibility(View.INVISIBLE);
+
+        img00.setImageResource(android.R.color.transparent);
+        img01.setImageResource(android.R.color.transparent);
+        img02.setImageResource(android.R.color.transparent);
+        img10.setImageResource(android.R.color.transparent);
+        img11.setImageResource(android.R.color.transparent);
+        img12.setImageResource(android.R.color.transparent);
+        img20.setImageResource(android.R.color.transparent);
+        img21.setImageResource(android.R.color.transparent);
+        img22.setImageResource(android.R.color.transparent);
+
     }
 
     public boolean endGame() {
         if (checkThree(values[0], values[1], values[2]) ||
-            checkThree(values[3], values[4], values[5]) ||
-            checkThree(values[6], values[7], values[8]))
-        {
+                checkThree(values[3], values[4], values[5]) ||
+                checkThree(values[6], values[7], values[8])) {
             Log.i("horizontal", "horizontal");
             return true;
 
         }
 
-        if (    checkThree(values[0], values[3], values[6]) ||
+        if (checkThree(values[0], values[3], values[6]) ||
                 checkThree(values[1], values[4], values[7]) ||
-                checkThree(values[2], values[5], values[8]))
-        {
+                checkThree(values[2], values[5], values[8])) {
             Log.i("vertical", "vertical");
             return true;
         }
-        if(
-            checkThree(values[0], values[4], values[8])||
-            checkThree(values[2], values[4], values[6]))
-        {
+        if (
+                checkThree(values[0], values[4], values[8]) ||
+                        checkThree(values[2], values[4], values[6])) {
             Log.i("diag", "diag");
             return true;
         }
         return false;
     }
 
-    public boolean checkThree(int one, int two, int three){
+    public boolean checkThree(int one, int two, int three) {
         if (one == 0) return false;
         return ((one == two) && (one == three));
     }
@@ -150,5 +162,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img00 = (ImageButton) findViewById(R.id.imageButton00);
+        img01 = (ImageButton) findViewById(R.id.imageButton01);
+        img02 = (ImageButton) findViewById(R.id.imageButton02);
+        img10 = (ImageButton) findViewById(R.id.imageButton10);
+        img11 = (ImageButton) findViewById(R.id.imageButton11);
+        img12 = (ImageButton) findViewById(R.id.imageButton12);
+        img20 = (ImageButton) findViewById(R.id.imageButton20);
+        img21 = (ImageButton) findViewById(R.id.imageButton21);
+        img22 = (ImageButton) findViewById(R.id.imageButton22);
+        TextView status = (TextView) findViewById(R.id.Title);
     }
 }
